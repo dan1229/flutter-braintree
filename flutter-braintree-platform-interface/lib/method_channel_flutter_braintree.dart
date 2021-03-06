ec8c9c39bf2a676ca824b468d631c07491c76107
Daniel Nazarian <naz1229@hotmail.com>
2021-03-06 16:15:03 -0500

    Started adding web support added flutter-braintree-platform-interface added flutter-braintree-web

---
+++ flutter-braintree-platform-interface/lib/method_channel_flutter_braintree.dart
@@ -0,0 +1,18 @@
+
+
+import 'flutter_braintree_platform_interface.dart';
+
+/// An implementation of [FlutterBraintreePlatform] that uses method channels.
+class MethodChannelFlutterBraintree extends FlutterBraintreePlatform {
+
+  @override
+  Future<void> init() {
+    return;
+  }
+
+  @override
+  Future<void> dispose() {
+    return;
+  }
+
+}
